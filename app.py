35f9419a3fb11f9f7a88f8292837a9a2d2492fd8
olubabajide <babajidehabeeb@yahoo.com>
2021-08-25 17:01:57 +0000

    save

--- app.py
+++ app.py
@@ -20,14 +20,14 @@ def check_card(func):
         data = request.get_json()
         if not data.get("status"):
             response = {"approved": False,
-            "newLimit": data.get("limit"),
-            "reason": "Blocked Card"}
+                        "newLimit": data.get("limit"),
+                        "reason": "Blocked Card"}
             return jsonify(response)
 
         if data.get("limit") < data.get("transaction").get("amount"):
             response = {"approved": False,
-            "newLimit": data.get("limit"),
-            "reason": "Transaction above the limit"}
+                        "newLimit": data.get("limit"),
+                        "reason": "Transaction above the limit"}
             return jsonify(response)
             return func(*args, **kwargs)
 
